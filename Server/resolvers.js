15c35196d68788955c625f64393f73b1ee970b98
sabithanazareth <41137183+sabithanazareth@users.noreply.github.com>
2023-11-12 11:08:53 -0500

    Basic structure for error throwing

--- Server/resolvers.js
+++ Server/resolvers.js
@@ -1,24 +1,12 @@
 import { GraphQLError } from "graphql";
-import validation from "./helpers.js";
-import redis from "redis";
-import moment from "moment";
-import { validate } from "uuid";
-import {
-  authors as authorCollection,
-  books as bookCollection,
-} from "./config/mongoCollections.js";
-
-import { v4 as uuid } from "uuid"; //Use uuid or mongo obj id?
-let client = redis.createClient();
-client.connect().then(() => {});
 
 export const resolvers = {
   //TODO: Add resolvers for typedefs
-  Query: {},
-  Parent: {},
-  Admin: {},
-  Nanny: {},
-  Listing: {},
-  Message: {},
-  Mutation: {},
+  // Query: {},
+  // Parent: {},
+  // Admin: {},
+  // Nanny: {},
+  // Listing: {},
+  // Message: {},
+  // Mutation: {},
 };
