d4da8c69c7424198782cf2d9849461a049beac68
sabithanazareth <41137183+sabithanazareth@users.noreply.github.com>
2023-11-12 11:13:51 -0500

    Uncommented resolvers

--- Server/resolvers.js
+++ Server/resolvers.js
@@ -2,11 +2,11 @@ import { GraphQLError } from "graphql";
 
 export const resolvers = {
   //TODO: Add resolvers for typedefs
-  // Query: {},
-  // Parent: {},
-  // Admin: {},
-  // Nanny: {},
-  // Listing: {},
-  // Message: {},
-  // Mutation: {},
+  Query: {},
+  Parent: {},
+  Admin: {},
+  Nanny: {},
+  Listing: {},
+  Message: {},
+  Mutation: {},
 };
