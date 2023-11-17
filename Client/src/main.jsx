60d134837fba63dc60a0d061cc28790548c680cf
sabithanazareth <41137183+sabithanazareth@users.noreply.github.com>
2023-11-16 21:54:50 -0500

    Updated Firebase config

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './index.css';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 //import fbconfig from './firebase/FirebaseConfig';
 //import {initializeApp} from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
@@ -10,32 +10,31 @@ import { getFirestore } from "firebase/firestore";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBKSPP32EYNW-pHl1ttsk2Dm3H7tadqFaI",
-  authDomain: "bookmynanny-30fb2.firebaseapp.com",
-  projectId: "bookmynanny-30fb2",
-  storageBucket: "bookmynanny-30fb2.appspot.com",
-  messagingSenderId: "1015093807568",
-  appId: "1:1015093807568:web:055109065af5d33acbd1d5"
+  apiKey: "AIzaSyDc97uKvteL8V3KLFgH6PkKM-7m8LhxN34",
+  authDomain: "bookmynanny-e879b.firebaseapp.com",
+  projectId: "bookmynanny-e879b",
+  storageBucket: "bookmynanny-e879b.appspot.com",
+  messagingSenderId: "1077833080815",
+  appId: "1:1077833080815:web:49bc0157f975763d86201a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
 // Add a new document in collection "cities"
 await setDoc(doc(db, "cities", "LA"), {
   name: "Los Angeles",
   state: "CA",
-  country: "USA"
+  country: "USA",
 });
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
