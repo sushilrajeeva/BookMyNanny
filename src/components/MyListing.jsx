3a559c3a55fba5bda127b0a76c855c578cbe386b
kavitha siratla <kavitha1469@gmail.com>
2023-11-18 14:55:43 -0500

    listings added

--- src/components/MyListing.jsx
+++ src/components/MyListing.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+
+
+
+import React, { useEffect, useState } from 'react';
+import { getAllListings } from '../firebase/ParentFunctions';
 
 function MyListing() {
-  return <div>MyListing</div>;
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    // Fetch all listings when the component mounts
+    const fetchListings = async () => {
+      try {
+        const allListings = await getAllListings();
+        setListings(allListings);
+        console.log(allListings)
+      } catch (error) {
+        console.error('Error fetching listings:', error);
+        // Handle the error appropriately (e.g., show an error message)
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  return (
+    <div>
+      <h1>All Listings</h1>
+      <ul>
+        {listings.map((listing) => (
+          <li key={listing.listingName}>
+            <strong>{listing.listingName}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MyListing;
--- src/components/PastListings.jsx
+++ src/components/PastListings.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import {getAllListings } from '../firebase/ParentFunctions';
 
 function PastListings() {
-  return <div>PastListings</div>;
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const allListings = await getAllListings();
+        setListings(allListings);
+      } catch (error) {
+        console.error('Error fetching listings:', error);
+        // Handle the error appropriately (e.g., show an error message)
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  const isListingPast = (endTime) => {
+    const currentDateTime = new Date();
+    const endTimeDate = endTime.toDate(); // Convert Firebase Timestamp to JavaScript Date object
+
+    // Compare the endTime with the current date and time
+    return endTimeDate < currentDateTime;
+  };
+  const activeListings = listings.filter((listing) =>
+  isListingPast(listing.endTime)
+  );
+
+  return (
+    <div>
+      <h1>Past Listings</h1>
+      <ul>
+        {activeListings.map((listing) => (
+          <li key={listing.listingName}>
+            <strong>{listing.listingName}</strong>
+            <p>{isListingPast(listing.endTime) ? 'Past' : 'Active'}</p>
+            {/* Add other listing details as needed */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PastListings;
