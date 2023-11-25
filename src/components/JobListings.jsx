2499cbb9c38f1c0d7e984b6b57aaeaa257cc1b07
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-11-24 20:49:12 -0500

    added I'm interested button to JobListings listing card
    This button will add the nannyID to the selected listings collections interestedNannies array. it will then update the card to disable the button when successfully executing the function

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { getAllListings } from "../firebase/NannyFunctions";
+import React, { useState, useEffect, useContext } from "react";
+
+import { getAllListings, nannyInterested } from "../firebase/NannyFunctions";
+import { AuthContext } from "../context/AuthContext";
 
 function formatFirestoreTimestamp(timestamp) {
   // Checking if timestamp is a Firestore Timestamp object
@@ -19,6 +21,7 @@ function JobListings() {
   const [jobListings, setJobListings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
+  const { currentUser, userRole } = useContext(AuthContext);
 
   useEffect(() => {
     async function fetchListings() {
@@ -38,7 +41,21 @@ function JobListings() {
     setSearchQuery(e.target.value.toString().toLowerCase());
   };
 
-  // logic for search
+  const handleNannyInterest = async (listingId) => {
+    const success = await nannyInterested(listingId, currentUser.uid);
+    if (success) {
+      console.log("Nanny added to InterestedNanny list successfully");
+      // Update only the modified listing
+      setJobListings(prevListings =>
+        prevListings.map(listing =>
+          listing._id === listingId
+            ? { ...listing, interestedNannies: [...listing.interestedNannies, currentUser.uid] }
+            : listing
+        )
+      );
+    }
+  };
+
   const filteredListings = jobListings.filter(listing => 
     listing.listingName.toLowerCase().includes(searchQuery) ||
     listing.description.toLowerCase().includes(searchQuery) ||
@@ -62,11 +79,36 @@ function JobListings() {
       />
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px' }}>
         {filteredListings.map((listing, index) => (
-          <div key={index} style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '8px', width: '300px' }}>
+          <div key={index} style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '8px', width: '300px', position: 'relative' }}>
             <h2>{listing.listingName}</h2>
-            <p><strong>Description:</strong> {listing.description}</p>
-            <p><strong>Posted Date:</strong> {formatFirestoreTimestamp(listing.postedDate)}</p>
-            <p><strong>Hourly Rate:</strong> {listing.hourlyRate}</p>
+            <div>
+              <p><strong>Description:</strong> {listing.description}</p>
+              <p><strong>Posted Date:</strong> {formatFirestoreTimestamp(listing.postedDate)}</p>
+              <p><strong>Hourly Rate:</strong> {listing.hourlyRate}</p>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+              <button 
+                onClick={() => console.log('View Listing is called')} 
+                style={{ backgroundColor: 'blue', color: 'white', padding: '10px 15px', borderRadius: '5px', flex: '1', marginRight: '5px' }}
+              >
+                View Listing
+              </button>
+              <button 
+                onClick={() => handleNannyInterest(listing._id)}
+                disabled={listing.interestedNannies && listing.interestedNannies.includes(currentUser.uid)}
+                style={{ 
+                  backgroundColor: listing.interestedNannies && listing.interestedNannies.includes(currentUser.uid) ? '#90EE90' : 'green', 
+                  color: 'white', 
+                  padding: '10px 15px', 
+                  borderRadius: '5px', 
+                  flex: '1', 
+                  marginLeft: '5px',
+                  cursor: listing.interestedNannies && listing.interestedNannies.includes(currentUser.uid) ? 'default' : 'pointer' 
+                }}
+              >
+                I'm Interested
+              </button>
+            </div>
           </div>
         ))}
       </div>
@@ -74,5 +116,5 @@ function JobListings() {
   );
 }
 
-
 export default JobListings;
+
