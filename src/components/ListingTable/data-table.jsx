1e71b1920abde31f93c306a28b4560d60696328d
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-12-02 12:32:07 -0500

    Added Pagination to Table

--- src/components/ListingTable/data-table.jsx
+++ src/components/ListingTable/data-table.jsx
@@ -7,21 +7,33 @@ import {
     flexRender,
     getCoreRowModel,
     getSortedRowModel,
+    getPaginationRowModel,
     useReactTable,
 } from "@tanstack/react-table";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
+
+
 const DataTable = ({ columns, data }) => {
     const [sorting, setSorting] = useState([]);
+    const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 10 });
+
+    
 
     const table = useReactTable({
         data,
         columns,
-        state: { sorting },
+        state: { sorting, pagination },
         onSortingChange: setSorting,
+        onPaginationChange: setPagination,
         getCoreRowModel: getCoreRowModel(),
         getSortedRowModel: getSortedRowModel(),
-    });
+        getPaginationRowModel: getPaginationRowModel(),
+      });
+
+    // Button styles
+    const buttonStyles = "px-3 py-1 text-sm font-medium text-white rounded hover:bg-blue-600";
+    const disabledButtonStyles = "px-3 py-1 text-sm text-gray-400 bg-gray-200 rounded cursor-not-allowed";
 
     return (
         <div className="rounded-md border">
@@ -49,6 +61,45 @@ const DataTable = ({ columns, data }) => {
                     ))}
                 </TableBody>
             </Table>
+
+            
+            {/* Pagination Controls */}
+      <div className="flex items-center justify-between px-2 py-2">
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        </div>
+        <div className="flex items-center space-x-2">
+          <button
+            onClick={() => table.setPageIndex(0)}
+            disabled={table.getState().pagination.pageIndex === 0}
+            className={table.getState().pagination.pageIndex === 0 ? disabledButtonStyles : buttonStyles + " bg-blue-500"}
+          >
+            First
+          </button>
+          <button
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+            className={!table.getCanPreviousPage() ? disabledButtonStyles : buttonStyles + " bg-blue-500"}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+            className={!table.getCanNextPage() ? disabledButtonStyles : buttonStyles + " bg-blue-500"}
+          >
+            Next
+          </button>
+          <button
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={table.getState().pagination.pageIndex === table.getPageCount() - 1}
+            className={table.getState().pagination.pageIndex === table.getPageCount() - 1 ? disabledButtonStyles : buttonStyles + " bg-blue-500"}
+          >
+            Last
+          </button>
+        </div>
+      </div>
+
         </div>
     );
 };
