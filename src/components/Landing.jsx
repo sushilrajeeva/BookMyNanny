719f39de3929b58cdebcededea7665d5c3e87eb0
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-12-02 02:23:20 -0500

    Updated Landing Page UI
    Added cards and hover effects on card

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,45 +1,60 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../App.css";
 
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 
 function Landing() {
+  const FeatureCard = ({ title, description }) => (
+    <Card className="mb-4 transition-transform duration-300 ease-in-out hover:shadow-lg hover:scale-105">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{description}</CardDescription>
+      </CardContent>
+    </Card>
+  );
+
   return (
-    <div className="card">
-      <div className="landing-header">
-        <h1>Welcome to BookMyNanny</h1>
-        <p>Your Trusted Nanny Booking Service</p>
+    <div className="max-w-4xl mx-auto p-4">
+      <div className="text-center mb-8">
+        <h1 className="text-3xl font-bold mb-2">Welcome to BookMyNanny</h1>
+        <p className="text-lg">Your Trusted Nanny Booking Service</p>
       </div>
 
-      <div className="landing-content">
-        <div className="feature">
-          <h2>Find Experienced Nannies</h2>
-          <p>Discover qualified and caring nannies for your children.</p>
-        </div>
+      <div className="grid md:grid-cols-2 gap-6">
+        <FeatureCard
+          title="Find Experienced Nannies"
+          description="Discover qualified and caring nannies for your children."
+        />
+        <FeatureCard
+          title="Easy Booking Process"
+          description="Effortlessly book the perfect nanny for your needs."
+        />
+        <FeatureCard
+          title="Safe and Reliable"
+          description="Our nannies undergo thorough background checks for your peace of mind."
+        />
+        <FeatureCard
+          title="Secure Payments"
+          description="Enjoy safe and secure payment methods for your bookings."
+        />
+      </div>
 
-        <div className="feature">
-          <h2>Easy Booking Process</h2>
-          <p>Effortlessly book the perfect nanny for your needs.</p>
-        </div>
-
-        <div className="feature">
-          <h2>Safe and Reliable</h2>
-          <p>
-            Our nannies undergo thorough background checks for your peace of
-            mind.
-          </p>
-        </div>
-
-        {/* "Sign Up" and "Sign In" buttons */}
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          <NavLink to="/signup" className="w-full">
-            <Button className="w-full">Sign Up</Button>
-          </NavLink>
-          <NavLink to="/signin" className="w-full">
-            <Button variant="secondary" className="w-full">Sign In</Button>
-          </NavLink>
-        </div>
+      <div className="flex justify-center gap-4 mt-8">
+        <NavLink to="/signup" className="w-full sm:w-auto">
+          <Button className="w-full sm:w-auto px-4 py-2">Sign Up</Button>
+        </NavLink>
+        <NavLink to="/signin" className="w-full sm:w-auto">
+          <Button variant="secondary" className="w-full sm:w-auto px-4 py-2">Sign In</Button>
+        </NavLink>
       </div>
     </div>
   );
