03d1dff668dd20abd964b0a44e4ea5914b98cc99
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-12-02 12:03:26 -0500

    Added support for sorting the columns
    Now the user can sort the table content by columns in ascending, descending or default state

--- src/components/ListingTable/columns.jsx
+++ src/components/ListingTable/columns.jsx
@@ -1,33 +1,51 @@
+// For this entire component i took direct reference from shadcn data-table
+// Reference - https://ui.shadcn.com/docs/components/data-table
+
 "use client";
 
-//import { ColumnDef } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
+
+import { ArrowUpDown } from "lucide-react";
+
+// Custom header component for sortable columns
+function DataTableColumnHeader({ column, title }) {
+  const isSortedDesc = column.getIsSorted() === 'desc';
+  const isSortedAsc = column.getIsSorted() === 'asc';
+
+  return (
+    <div onClick={() => column.toggleSorting()} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+      {title}
+      <ArrowUpDown className={isSortedDesc ? "ml-2" : isSortedAsc ? "rotate-180 ml-2" : "ml-2"} />
+    </div>
+  );
+}
 
 export const columns = [
   {
     accessorKey: "listingName",
-    header: "Listing Name",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Listing Name" />,
   },
   {
-    accessorKey: "location",
+    id: "location",
     header: "Location",
     cell: (info) => `${info.row.original.street}, ${info.row.original.city}, ${info.row.original.state}, ${info.row.original.country} - ${info.row.original.pincode}`,
+    enableSorting: false, // doing this to disable sorting as i don't want sorting on location
   },
   {
     accessorKey: "hourlyRate",
-    header: "Hourly Rate",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Hourly Rate" />,
   },
   {
     accessorKey: "jobStartDate",
-    header: "Start Date",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Start Date" />,
   },
   {
     accessorKey: "jobEndDate",
-    header: "End Date",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="End Date" />,
   },
   {
     id: 'action',
     header: "Action",
-    cell: (info) => <Button onClick={() => console.log('View job', info.row.original._id)}>View Job</Button>,
+    cell: (info) => <div onClick={() => console.log('View job', info.row.original._id)} style={{ cursor: 'pointer' }}>View Job</div>,
+    enableSorting: false, // doing this to disable sorting as i don't want sorting on button actions
   },
 ];
--- src/components/ListingTable/data-table.jsx
+++ src/components/ListingTable/data-table.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table';
+// For this entire component i took direct reference from shadcn data-table
+// Reference - https://ui.shadcn.com/docs/components/data-table
+
+
+import React, { useState } from 'react';
+import {
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 const DataTable = ({ columns, data }) => {
+    const [sorting, setSorting] = useState([]);
+
     const table = useReactTable({
         data,
         columns,
+        state: { sorting },
+        onSortingChange: setSorting,
         getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
     });
 
     return (
@@ -15,39 +29,24 @@ const DataTable = ({ columns, data }) => {
                 <TableHeader>
                     {table.getHeaderGroups().map((headerGroup) => (
                         <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                );
-                            })}
+                            {headerGroup.headers.map((header) => (
+                                <TableHead key={header.id}>
+                                    {flexRender(header.column.columnDef.header, header.getContext())}
+                                </TableHead>
+                            ))}
                         </TableRow>
                     ))}
                 </TableHeader>
                 <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
+                    {table.getRowModel().rows.map((row) => (
+                        <TableRow key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <TableCell key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </TableCell>
+                            ))}
                         </TableRow>
-                    )}
+                    ))}
                 </TableBody>
             </Table>
         </div>
