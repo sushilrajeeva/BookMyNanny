136826962b3c209be7363dae60846be43986fad7
sabithanazareth <41137183+sabithanazareth@users.noreply.github.com>
2023-12-16 22:20:01 -0500

    Job Completion

--- src/components/JobCompletion.jsx
+++ src/components/JobCompletion.jsx
@@ -1,13 +1,12 @@
-import React, { useState , useContext } from "react";
+import React, { useState, useContext } from "react";
 import { onJobComplete } from "@/firebase/NannyFunctions";
 import { AuthContext } from "@/context/AuthContext";
-import {checkNumber} from "../helpers/index.js"
-
+import { checkNumber } from "../helpers/index.js";
 
 function JobCompletion({ listing }) {
   const [showForm, setShowForm] = useState(false);
   const [hoursWorked, setHoursWorked] = useState(0);
-  const auth = useContext(AuthContext)
+  const auth = useContext(AuthContext);
 
   const handleCompleteJob = () => {
     setShowForm(true);
@@ -16,15 +15,15 @@ function JobCompletion({ listing }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-    checkNumber(hoursWorked,"hours worked")
-    await onJobComplete(listing._id, hoursWorked, auth.currentUser.uid);
-    // Reset state
-    setHoursWorked(0);
-    setShowForm(false);
+      checkNumber(parseFloat(hoursWorked), "hours worked");
+      console.log("LID", listing._id);
+      await onJobComplete(listing._id, hoursWorked, auth.currentUser.uid);
+      // Reset state
+      setHoursWorked(0);
+      setShowForm(false);
     } catch (error) {
-      console.error(`Error completing job: ${error}`)
+      console.error(`Error completing job: ${error}`);
     }
-    
   };
 
   return (
