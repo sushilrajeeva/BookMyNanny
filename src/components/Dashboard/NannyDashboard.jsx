deb3a35d76abe3e4c8499e1733adc1ac5bf9e204
sabithanazareth <41137183+sabithanazareth@users.noreply.github.com>
2023-12-17 21:53:59 -0500

    NotVerified

--- src/components/Dashboard/NannyDashboard.jsx
+++ src/components/Dashboard/NannyDashboard.jsx
@@ -1,42 +1,74 @@
-import React, { useState } from "react";
-import ActiveJobs from "../NannyComponent/ActiveJobs"
+import React, { useEffect, useState, useContext } from "react";
+import ActiveJobs from "../NannyComponent/ActiveJobs";
 import PastJobs from "../NannyComponent/PastJobs";
 import JobListings from "../NannyComponent/JobListings";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import { getNannyById } from "@/firebase/NannyFunctions";
+import { AuthContext } from "../../context/AuthContext";
+import NotVerified from "../NannyComponent/NotVerified";
 
-const NannyDashboard = ({ userRole }) => {
+const NannyDashboard = () => {
+  const { currentUser } = useContext(AuthContext);
   const [activeComponent, setActiveComponent] = useState("JobListings");
+  const [nannyData, setNannyData] = useState(null);
 
   const renderComponent = (component) => {
     setActiveComponent(component);
   };
 
+  useEffect(() => {
+    const fetchNanny = async () => {
+      try {
+        const nannyDetails = await getNannyById(currentUser.uid);
+        setNannyData(nannyDetails);
+      } catch (error) {
+        console.error("Error fetching nanny:", error);
+      }
+    };
+
+    fetchNanny();
+  }, [currentUser]);
+
   return (
     <Container maxWidth="lg" className="dashboard">
-      <Paper elevation={3} className="navigation">
-        <Box>
+      {nannyData !== null ? ( // Check if nannyData is not null
+        nannyData.verified === true ? (
           <>
-            <Button onClick={() => renderComponent("JobListings")}>
-              Job Listings
-            </Button>
-            <Button onClick={() => renderComponent("ActiveJobs")}>
-              Active Jobs
-            </Button>
-            <Button onClick={() => renderComponent("PastJobs")}>
-              Past Jobs
-            </Button>
-          </>
-        </Box>
-      </Paper>
+            <Paper elevation={3} className="navigation">
+              <Box>
+                <>
+                  <Button onClick={() => renderComponent("JobListings")}>
+                    Job Listings
+                  </Button>
+                  <Button onClick={() => renderComponent("ActiveJobs")}>
+                    Active Jobs
+                  </Button>
+                  <Button onClick={() => renderComponent("PastJobs")}>
+                    Past Jobs
+                  </Button>
+                </>
+              </Box>
+            </Paper>
 
-      <Paper elevation={3} className="component-container">
-        {activeComponent === "ActiveJobs" && <ActiveJobs />}
-        {activeComponent === "PastJobs" && <PastJobs />}
-        {activeComponent === "JobListings" && <JobListings />}
-      </Paper>
+            <Paper elevation={3} className="component-container">
+              {activeComponent === "ActiveJobs" && <ActiveJobs />}
+              {activeComponent === "PastJobs" && <PastJobs />}
+              {activeComponent === "JobListings" && <JobListings />}
+            </Paper>
+          </>
+        ) : (
+          <Paper elevation={3} className="not-verified">
+            <Box>
+              <NotVerified />
+            </Box>
+          </Paper>
+        )
+      ) : (
+        <div>Loading</div>
+      )}
     </Container>
   );
 };
---
+++ src/components/NannyComponent/NotVerified.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { doSignOut } from "../../firebase/AuthFunctions";
+
+function NotVerified() {
+  const handleLogout = () => {
+    // Call the doSignOut function here
+    doSignOut();
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-[350px] shadow-md">
+        <CardHeader>
+          <CardTitle className="text-xl font-bold">
+            Verification Pending
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="text-center">
+          <p className="text-gray-600">
+            You are not verified. Please wait for admin approval.
+          </p>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button onClick={handleLogout}>Logout</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default NotVerified;
