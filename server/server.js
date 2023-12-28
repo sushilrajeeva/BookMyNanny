4fe7db7958cc526a1257fc32ca025541ef996774
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-12-27 23:24:19 -0500

    Added Stripe payment Support

--- server/server.js
+++ server/server.js
@@ -42,6 +42,51 @@ dotenv.config();
 const app = express();
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
+
+const endpointSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
+// Reference -> https://dashboard.stripe.com/test/webhooks/create?endpoint_location=local
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body, // Raw body is used here
+        sig,
+        endpointSecret
+      );
+    } catch (err) {
+      console.error("Error:", err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Check if the event type is 'checkout.session.completed'
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      const listingID = session.metadata.listingID; // Extract the listingID from session metadata
+
+      try {
+        // Perform your business logic here
+        await jobClose(listingID);
+        console.log("Job closed successfully for listing:", listingID);
+        return res.status(200).json({ received: true, closed: listingID });
+      } catch (error) {
+        // Error handling for your business logic
+        console.error("Error in jobClose:", error);
+        return res.status(500).send(`Error closing job: ${error}`);
+      }
+    } else {
+      // Handling other event types or unhandled events
+      console.log("Received unhandled event type:", event.type);
+      return res
+        .status(200)
+        .json({ received: true, unhandled_event: event.type });
+    }
+  }
+);
 app.use(express.json());
 
 app.get("/s3Url", async (req, res) => {
@@ -53,6 +98,12 @@ app.post("/api/create-checkout-session", async (req, res) => {
   try {
     const { transaction } = req.body;
     let totalAmount = 0;
+    const payerID = transaction[0].payerID;
+    const payeeID = transaction[0].payeeID;
+    const listingID = transaction[0].listingID;
+    console.log("payerID", payerID);
+    console.log("payeeID", payeeID);
+    console.log("listingID", listingID);
     const lineItems = transaction.map((product) => ({
       price_data: {
         currency: "usd",
@@ -67,9 +118,12 @@ app.post("/api/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.CLIENT_URL}/paymentSuccess`,
+      metadata: { listingID: listingID },
+      success_url: `${process.env.CLIENT_URL}/PaymentSuccessPage`,
       cancel_url: `${process.env.CLIENT_URL}/paymentFailure`,
     });
+
+    console.log("stupppdaflkajf");
 
     return res.json({ id: session.id });
   } catch (error) {
@@ -79,6 +133,27 @@ app.post("/api/create-checkout-session", async (req, res) => {
       .json({ error: "Internal Server Error", msg: error.message });
   }
 });
+
+async function jobClose(listingId) {
+  try {
+    const listingDocRef = doc(db, "Listings", listingId);
+    const listing = await getDoc(listingDocRef);
+
+    if (listing) {
+      const { progressBar } = listing.data();
+      if (progressBar === 0) {
+        await updateDoc(listingDocRef, { progressBar: 100 });
+      } else {
+        throw "Job is already completed.";
+      }
+    } else {
+      throw "Listing does not exist.";
+    }
+  } catch (error) {
+    console.error("Error verifying job:", error);
+    return null;
+  }
+}
 
 async function getNannyById(id) {
   try {
