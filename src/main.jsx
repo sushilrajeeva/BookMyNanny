33f881e6feafd02b649cf2afe69f32bfe392e3be
Sai Pranav <113039239+saipranav789@users.noreply.github.com>
2024-08-08 09:37:48 -0400

    Update main.jsx
    Removed comments

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,6 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { doc, setDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDc97uKvteL8V3KLFgH6PkKM-7m8LhxN34",
-//   authDomain: "bookmynanny-e879b.firebaseapp.com",
-//   projectId: "bookmynanny-e879b",
-//   storageBucket: "bookmynanny-e879b.appspot.com",
-//   messagingSenderId: "1077833080815",
-//   appId: "1:1077833080815:web:49bc0157f975763d86201a",
-// };
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
